420fa684ec258b0c9a4376a648e318afffa384db
xiaoxin.cao <1216099190@qq.com>
2017-05-31 14:25:22 +0800

    Initial Commit

---
+++ GitManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitManager
+//
+//  Created by cying on 17/5/31.
+//  Copyright © 2017年 saiyunjiuzhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitManager
+//
+//  Created by cying on 17/5/31.
+//  Copyright © 2017年 saiyunjiuzhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
