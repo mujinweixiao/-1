d43c68898a9163c2b7d45a9709c656bb45813ab1
xiaoxin.cao <1216099190@qq.com>
2017-05-31 15:15:49 +0800

    添加one文件

---
+++ GitManager/One.h
@@ -0,0 +1,13 @@
+//
+//  One.h
+//  GitManager
+//
+//  Created by cying on 17/5/31.
+//  Copyright © 2017年 saiyunjiuzhou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface One : NSObject
+
+@end
---
+++ GitManager/One.m
@@ -0,0 +1,13 @@
+//
+//  One.m
+//  GitManager
+//
+//  Created by cying on 17/5/31.
+//  Copyright © 2017年 saiyunjiuzhou. All rights reserved.
+//
+
+#import "One.h"
+
+@implementation One
+
+@end
